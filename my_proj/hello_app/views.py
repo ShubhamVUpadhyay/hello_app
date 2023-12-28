7bff026fb4e648a2fa0857bd29cd459a394f8a38
Shubham Upadhyay <103402465+ShubhamVUpadhyay@users.noreply.github.com>
2023-12-28 14:12:30 +0530

    Update views.py

--- my_proj/hello_app/views.py
+++ my_proj/hello_app/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def Hello(request):
   return HttpResponse('Hello')
+# Create your views here.
+def Home(request):
+  return HttpResponse('it is Home')
