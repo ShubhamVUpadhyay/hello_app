5f8d71531c80f164aa52e0b0e094a93c97ec4af8
Shubham Upadhyay <103402465+ShubhamVUpadhyay@users.noreply.github.com>
2023-12-28 13:50:07 +0530

    Update urls.py

--- my_proj/hello_app/urls.py
+++ my_proj/hello_app/urls.py
@@ -3,6 +3,5 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('hello/',views.Hello,name='hello')
+    path('',views.Hello,name='hello')
 ]
